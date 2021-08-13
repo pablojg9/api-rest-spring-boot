67b271d259d5fdd0145e83fab8b06bef3af99ee6
pablojg9 <pablojuniorgn2@gmail.com>
2021-08-13 20:46:24 -0300

    api rest feito com spring boot

--- src/main/java/com/project/api/rest/controller/ClientController.java
+++ src/main/java/com/project/api/rest/controller/ClientController.java
@@ -5,7 +5,6 @@ import com.project.api.rest.repository.ClientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
